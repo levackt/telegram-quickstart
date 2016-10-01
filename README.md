c5e88205ab39d28f6b4eaa3d31cd56b88a69ee6d
Taariq Levack <taariql@gmail.com>
2016-10-01 08:33:16 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# telegram-quickstart
-A quickstart to create a telegram Bot based on Apache Camel.
+# telegram-webcam-quickstart
 
-The quickstart can work without any modification to the code if you just put your Telegram authorization token into the [application.properties](/src/main/resources/application.properties) file.
+Please check out Nicola Ferraro's [blog post] (https://www.nicolaferraro.me/2016/05/27/creating-a-telegram-bot-in-5-minutes-with-apache-camel/) for the vanilla telegram bot.
+
+This fork is for a Java Meetup demo I recently did, showing a Telegram bot posting images from a Raspberry Pi.
+
+We also talked a lot about Camel routing, for more of that check out Claus Ibsen's [latest talks at JDK.IO 2016] (http://www.davsclaus.com/2016/09/videos-from-my-two-latest-talks-about.html)
+
+The quickstart can work without any modification to the code if you just put your Telegram authorization token into the [application.yml](/src/main/resources/application.yml) file.
+
+To get an image from your webcam, type /cam
+
+![alt](images/upsidedownpatientphotogenicguy.png)
+
 
 You can change the [Bot](src/main/java/com/example/Bot.java) class to customize the behavior.
+
+# build
+You can also package and run the fatjar as follows;
+
+    mvn clean install
+
+    java -jar telegram-webcam-quickstart-1.0.0-SNAPSHOT.jar
+
+
+
+
--- src/main/java/com/example/CamelRouter.java
+++ src/main/java/com/example/CamelRouter.java
@@ -1,11 +1,13 @@
 package com.example;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.telegram.TelegramConstants;
+import org.apache.camel.component.telegram.TelegramMediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * This class is responsible for routing the messages from and to the Telegram chat.
+ * This class is responsible for routing the messages from and to the Telegram chat, as well polling a webcam.
  */
 @Component
 public class CamelRouter extends RouteBuilder {
@@ -16,9 +18,26 @@ public class CamelRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        //Telegram bot route
         from("telegram:bots/{{telegram.auth.token}}")
-        .bean(bot)
+                .choice().
+                    when(body().isNull())
+                    .log("No body out there")
+                    .endChoice()
+                    .when(body().isEqualTo("do-not-reply"))
+                        .log("OK, won't reply")
+                    .endChoice()
+                    .when(body().isEqualTo("/cam"))
+                        .to("direct:cam")
+                        .setHeader(TelegramConstants.TELEGRAM_MEDIA_TYPE, constant(TelegramMediaType.PHOTO_JPG))
+                        .setHeader(TelegramConstants.TELEGRAM_MEDIA_TITLE_CAPTION, constant("Aw yiss"))
+                    .endChoice()
+                    .otherwise().bean(bot)
+                .end()
         .to("telegram:bots/{{telegram.auth.token}}");
+
+        //Web cam route, throttling to 1 per/second for demo...
+        from("direct:cam").throttle(1).to("webcam:foo");
 
     }
 }
